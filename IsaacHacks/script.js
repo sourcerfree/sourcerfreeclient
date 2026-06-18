e6fd8c95f85846548f7504c4054c86d7d6316ce7
sourcerfree <isakinhogamesplays7@gmail.com>
2026-06-18 20:36:38 -0300

    Update script.js

--- IsaacHacks/script.js
+++ IsaacHacks/script.js
@@ -1,10 +1,10 @@
 // script.js
 const versions = {
-  'v1.0': { name: 'Client v1.0', url: 'https://raw.githubusercontent.com/github/gitignore/main/README.md', filename: 'SourcerFreeInstaller-v1.0.exe' },
-  'v2.0': { name: 'Client v2.0', url: 'https://raw.githubusercontent.com/github/gitignore/main/LICENSE', filename: 'SourcerFreeInstaller-v2.0.exe' },
-  'v3.0': { name: 'Client v3.0', url: 'https://raw.githubusercontent.com/github/gitignore/main/README.md', filename: 'SourcerFreeInstaller-v3.0.exe' },
-  'v4.0': { name: 'Client v4.0', url: 'https://raw.githubusercontent.com/github/gitignore/main/LICENSE', filename: 'SourcerFreeInstaller-v4.0.exe' },
-  'v5.0': { name: 'Client v5.0', url: 'https://raw.githubusercontent.com/github/gitignore/main/README.md', filename: 'SourcerFreeInstaller-v5.0.exe' }
+  'v1.0': { name: 'Client v1.0', url: 'https://raw.githubusercontent.com/sourcerfree/sourcerfreeclient/refs/heads/main/SourcerFreeInstaller.exe', filename: 'SourcerFreeInstaller-v1.0.exe' },
+  'v2.0': { name: 'Client v2.0', url: 'https://raw.githubusercontent.com/sourcerfree/sourcerfreeclient/refs/heads/main/SourcerFreeInstaller.exe', filename: 'SourcerFreeInstaller-v2.0.exe' },
+  'v3.0': { name: 'Client v3.0', url: 'https://raw.githubusercontent.com/sourcerfree/sourcerfreeclient/refs/heads/main/SourcerFreeInstaller.exe', filename: 'SourcerFreeInstaller-v3.0.exe' },
+  'v4.0': { name: 'Client v4.0', url: 'https://raw.githubusercontent.com/sourcerfree/sourcerfreeclient/refs/heads/main/SourcerFreeInstaller.exe', filename: 'SourcerFreeInstaller-v4.0.exe' },
+  'v5.0': { name: 'Client v5.0', url: 'https://raw.githubusercontent.comsourcerfree/sourcerfreeclient/refs/heads/main/SourcerFreeInstaller.exe', filename: 'SourcerFreeInstaller-v5.0.exe' }
 };
 
 document.addEventListener('DOMContentLoaded',()=>{
